9d31d15b66da819b8c579bfae113325f8162e112
Kirby Cool <kirbydcool@gmail.com>
2013-11-20 20:20:28 -0800

    added the button hook

--- chrome-ext/gmail-injector.js
+++ chrome-ext/gmail-injector.js
@@ -1,10 +1,39 @@
-function addButtonHooks()
-{
-    var divs = $("div");
-    divs.each(function (t) {
-        if (t.html() === "Send") {
-            alert("Found send button!"
-    }
+
+function addButtonHooks() {
+
+  // Find send button
+  // TODO: look for a better way to do this
+  var sendButton;
+  var sendDivs = $("div:contains('Send'):not(:has('div'))");
+  if ( sendDivs.length == 1 ) {
+    sendButton = sendDivs[0];
+  else {
+    //TODO: some useful error handling
+    ;
+  }
+  
+  // Inject our js
+
+  //clones the button. the dummy button has no listeners
+  var dummySendButton = sendButton.cloneNode();
+  //Replace the send button with our dummy button
+  sendButton.parent.replaceChild( dummySendButton, sendButton );
+
+  // Now we can add whatever we want to dummySendButton.onclick and then call
+  // sendButton.click() to perform the normal click procedure
+  dummySendButton.onclick = function () {
+    encryptEmail();
+    alert( "running super awesome encrypted email code" );
+
+    sendButton.click();
+  }
+}
+
+// Performs all of the encryption tasks before continuing the normal
+// Gmail send function.
+function encryptEmail() {
+  //TODO: implement this
+  return;
 }
 
 // Called when the url of a tab changes.
