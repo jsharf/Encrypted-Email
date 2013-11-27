f346c389f4e0ab2422367e7d7e3ec4a868ce174c
Kirby Cool <kirbydcool@gmail.com>
2013-11-27 00:42:56 -0800

    working on implementing the chrome extension. work in progress.

--- chrome-ext/gmail-injector.js
+++ chrome-ext/gmail-injector.js
@@ -1,3 +1,5 @@
+var keyServerURL = "google.com"; //TODO: figure this out
+
 
 function addButtonHooks() {
 
@@ -5,43 +7,69 @@ function addButtonHooks() {
     // TODO: look for a better way to do this
     var sendButton;
     var sendDivs = $("div:contains('Send'):not(:has('div'))");
-    if ( sendDivs.length > 0 ) {
+    if ( sendDivs.length <= 0 ) {
+      // TODO: some useful error
+      return;
+    }
+
+    for (var i=0; i < sendDivs.length; i++) {
+      sendButton = sendDivs[i];
+    
+      // if the button isn't already a dummy button
+      if (!sendButton.EncryptedEmailDummy) {
+        // Inject our js
+          
+        //clones the button. the dummy button has no listeners
+        var dummySendButton = sendButton.cloneNode();
         
-        for (var i=0; i<sendDivs.length; i++) {
-            sendButton = sendDivs[i];
-	    // if the button isn't already a dummy button
-        if (!sendButton.EncryptedEmailDummy) {
-	        // Inject our js
-                
-		//clones the button. the dummy button has no listeners
-            	var dummySendButton = sendButton.cloneNode();
-            	
-		//Replace the send button with our dummy button
-            	sendButton.parentNode.replaceChild( dummySendButton, sendButton );
+        //Replace the send button with our dummy button
+        sendButton.parentNode.replaceChild( dummySendButton, sendButton );
 
-            	// Now we can add whatever we want to dummySendButton.onclick and then call
-            	// sendButton.click() to perform the normal click procedure
-            	dummySendButton.onclick = function () {
-                    encryptEmail();
-                    alert( "running super awesome encrypted email code" );
-                    sendButton.click();
-                }
-            // mark the newly created button as a dummy button
-	        dummySendButton.EncryptedEmailDummy = true;
-	    }
-        }
+        // Now we can add whatever we want to dummySendButton.onclick
+        // and then call sendButton.click()
+        // to perform the normal click procedure
+        dummySendButton.onclick = function () {
+            encryptEmail();
+            alert( "running super awesome encrypted email code" );
+            sendButton.click();
+        };
+        // mark the newly created button as a dummy button
+        dummySendButton.EncryptedEmailDummy = true;
+      }
     }
-    else {
-        //TODO: some useful error handling
-        ;
-    }
-
 }
 
 // Performs all of the encryption tasks before continuing the normal
 // Gmail send function.
 function encryptEmail() {
+
     //TODO: implement this
+    var messageDiv = $("div[role='textbox'][aria-label='Message Body']");
+    var message = messageDiv.html();
+    console.log(message);   
+
+    //get the key
+    var request = $.ajax({
+      type: "GET",
+      url: keyServerURL,
+      data: { 'email': $("#toAddress").val()}
+    });
+
+    request.done( function( data ) {
+      var parsedData = $.parseJSON(data);
+      var rPubKey = parsedData.publickey;
+      //Encrypt
+      openpgp.init();
+      var pubKey = openpgp.read_publicKey( rPubKey );
+      var encryptedMessage = openpgp.write_encrypted_message( pubKey, message );
+      messageDiv.html(message);
+    });
+
+    request.fail( function() {
+      //TODO: some useful error
+      return;
+    });     
+
     return;
 }
 
